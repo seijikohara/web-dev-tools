db92a14905507ed64c4fade4774f2c9047abb1a3
Seiji Kohara <sage.0118@gmail.com>
2020-10-07 13:09:33 +0900

    Dashboard画面の機能を拡張
    - Geo追加
    - RDAP追加
    - レイアウトの見直し

--- src/main/kotlin/net/relaxism/devtools/webdevtools/config/RoutingConfiguration.kt
+++ src/main/kotlin/net/relaxism/devtools/webdevtools/config/RoutingConfiguration.kt
@@ -1,6 +1,9 @@
 package net.relaxism.devtools.webdevtools.config
 
+import net.relaxism.devtools.webdevtools.handler.GeoApiHandler
 import net.relaxism.devtools.webdevtools.handler.IpApiHandler
+import net.relaxism.devtools.webdevtools.handler.RdapApiHandler
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
@@ -8,12 +11,18 @@ import org.springframework.web.reactive.function.server.router
 
 
 @Configuration
-class RoutingConfiguration(private val ipApiHandler: IpApiHandler) {
+class RoutingConfiguration(
+    @Autowired private val ipApiHandler: IpApiHandler,
+    @Autowired private val rdapApiHandler: RdapApiHandler,
+    @Autowired private val geoApiHandler: GeoApiHandler
+) {
 
     @Bean
     fun apiRouter() = router {
         accept(MediaType.ALL).nest {
             GET("/api/ip", ipApiHandler::getIp)
+            GET("/api/rdap/{ip}", rdapApiHandler::getRdap)
+            GET("/api/geo/{ip}", geoApiHandler::getGeo)
         }
     }
 
