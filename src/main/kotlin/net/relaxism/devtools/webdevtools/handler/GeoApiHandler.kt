ab0d126b7e169d318b15d195d69a2f266941b218
Seiji Kohara <sage.0118@gmail.com>
2020-10-31 18:58:14 +0900

    外部APIの呼び出しをService化

--- src/main/kotlin/net/relaxism/devtools/webdevtools/handler/GeoApiHandler.kt
+++ src/main/kotlin/net/relaxism/devtools/webdevtools/handler/GeoApiHandler.kt
@@ -1,12 +1,11 @@
 package net.relaxism.devtools.webdevtools.handler
 
 import net.relaxism.devtools.webdevtools.config.ApplicationProperties
-import net.relaxism.devtools.webdevtools.utils.JsonUtils
+import net.relaxism.devtools.webdevtools.service.ExternalJsonApiService
 import org.slf4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestTemplate
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
@@ -15,16 +14,16 @@ import reactor.core.publisher.Mono
 class GeoApiHandler(
     @Autowired val logger: Logger,
     @Autowired val applicationProperties: ApplicationProperties,
-    @Autowired val restTemplate: RestTemplate
-) : ExternalApiCaller {
+    @Autowired val externalJsonApiService: ExternalJsonApiService
+) {
 
     fun getGeo(request: ServerRequest): Mono<ServerResponse> {
         val ipAddress = request.pathVariable("ip")
         val uri = "${applicationProperties.network.geo.uri}/${ipAddress}";
         logger.info("[GEO] ${uri}")
-        val geoJson = callExternalApi(restTemplate, uri)
+        val geoJson = externalJsonApiService.get(uri)
 
-        val response = Response(JsonUtils.fromJson(geoJson))
+        val response = Response(geoJson)
         return ServerResponse.ok()
             .contentType(MediaType.APPLICATION_JSON)
             .body(Mono.just(response), Response::class.java)
