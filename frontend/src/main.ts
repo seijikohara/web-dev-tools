464ea41c4ba790cf72bff6bf808c9c0e3fd973b4
Seiji Kohara <sage.0118@gmail.com>
2020-11-23 10:20:22 +0900

    GoogleAnalyticsの導入、依存関係の更新

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import VueGtag from "vue-gtag-next";
 import App from "./App.vue";
 import { store } from "./store";
 import router from "./router";
@@ -6,6 +7,18 @@ import router from "./router";
 import "@/assets/scss/layout.scss";
 
 const app = createApp(App);
-app.use(store).use(router).mount("#app");
+app.use(store);
+app.use(router);
+
+const gaMeasurementId = process.env.VUE_APP_GA_MEASUREMENT_ID;
+if (gaMeasurementId) {
+  app.use(VueGtag, {
+    property: {
+      id: gaMeasurementId,
+    },
+  });
+}
+
+app.mount("#app");
 
 app.config.globalProperties.$primevue = { ripple: true };
