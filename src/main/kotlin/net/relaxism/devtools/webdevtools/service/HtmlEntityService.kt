cd8cf0992017b806a5433b4f9e7b8fa13b6d0e22
Seiji Kohara <sage.0118@gmail.com>
2020-11-26 23:47:51 +0900

    Apply paging to HTML entity reference Vue page

--- src/main/kotlin/net/relaxism/devtools/webdevtools/service/HtmlEntityService.kt
+++ src/main/kotlin/net/relaxism/devtools/webdevtools/service/HtmlEntityService.kt
@@ -3,12 +3,23 @@ package net.relaxism.devtools.webdevtools.service
 import net.relaxism.devtools.webdevtools.repository.HtmlEntity
 import net.relaxism.devtools.webdevtools.repository.HtmlEntityRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Service
 class HtmlEntityService(@Autowired private val htmlEntityRepository: HtmlEntityRepository) {
 
     fun findAll(): Flux<HtmlEntity> = htmlEntityRepository.findAll()
+
+    fun findByNameContaining(name: String, pageable: Pageable): Mono<Page<HtmlEntity>> {
+        return Mono.zip(
+            htmlEntityRepository.findByNameContaining(name, pageable).collectList(),
+            htmlEntityRepository.countByNameContaining(name)
+        ).map { PageImpl(it.t1, pageable, it.t2) }
+    }
 
 }
