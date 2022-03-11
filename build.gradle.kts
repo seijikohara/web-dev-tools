1004c9ae7235a2db42721c8c6d8f0fde36cfe585
Seiji Kohara <sage.0118@gmail.com>
2022-03-11 21:24:52 +0900

    Upgrade Gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.github.seancfoley:ipaddress:5.3.3")
-    implementation("com.google.guava:guava:31.0.1-jre")
+    implementation("com.google.guava:guava:31.1-jre")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.flywaydb:flyway-core")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
