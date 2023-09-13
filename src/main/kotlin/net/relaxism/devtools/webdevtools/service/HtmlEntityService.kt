4a864be7eb4b1ffedd7c82a65c907dc30e7a33c9
Seiji Kohara <sage.0118@gmail.com>
2023-09-13 22:01:47 +0900

    Reformat code

--- src/main/kotlin/net/relaxism/devtools/webdevtools/service/HtmlEntityService.kt
+++ src/main/kotlin/net/relaxism/devtools/webdevtools/service/HtmlEntityService.kt
@@ -13,7 +13,6 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
-import reactor.core.publisher.Mono
 
 @Service
 class HtmlEntityService(
