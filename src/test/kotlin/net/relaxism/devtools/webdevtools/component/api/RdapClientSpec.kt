34c6287e876479639bdbeda2e2fccbcbdf608fd3
Seiji Kohara <sage.0118@gmail.com>
2024-01-21 11:14:44 +0900

    Remove redundant annotations

--- src/test/kotlin/net/relaxism/devtools/webdevtools/component/api/RdapClientSpec.kt
+++ src/test/kotlin/net/relaxism/devtools/webdevtools/component/api/RdapClientSpec.kt
@@ -3,13 +3,11 @@ package net.relaxism.devtools.webdevtools.component.api
 import io.kotest.assertions.json.shouldContainJsonKey
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.json.JSONObject
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
-@OptIn(ExperimentalCoroutinesApi::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class RdapClientSpec(
     @Autowired private val rdapClient: RdapClient,
