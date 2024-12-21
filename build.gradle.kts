b142996c00d9e3ddf3b67ab64975fec5c9dbf254
Seiji Kohara <sage.0118@gmail.com>
2024-12-21 23:28:06 +0900

    Upgrade Gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 plugins {
     id("project-report")
     id("com.github.node-gradle.node") version "7.1.0"
-    id("org.springframework.boot") version "3.4.0"
+    id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("jvm") version "2.0.21"
     kotlin("plugin.spring") version "2.0.21"
